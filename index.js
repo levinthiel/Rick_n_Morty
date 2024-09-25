8a3c219488021a991e41c5db04cd4a11d534b642
Bianca <gundel.biancaw@web.de>
2024-09-25 12:12:27 +0200

    add fetchCharacters function and fixed CharacterCard function

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  cardContainer.innerHTML = "";
+  const endpoint = "https://rickandmortyapi.com/api/character?page=1";
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  const characters = data.results;
+  characters.forEach((character) => {
+    const src = character.image;
+    const name = character.name;
+    const status = character.status;
+    const type = character.type;
+    const occ = character.episode.length;
+    const newCharacterCard = CharacterCard(src, name, status, type, occ);
+    cardContainer.append(newCharacterCard);
+  });
+}
+
+fetchCharacters();
